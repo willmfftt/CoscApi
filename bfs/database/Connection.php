9eb92cb186e5ad07fcefe6b46e3ec8a86ae586d4
William Moffitt <w.moffitt@wmoffitt.com>
2015-10-14 19:11:20 -0400

    Added database connection files

---
+++ bfs/database/Connection.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Bfs\Database;
+
+require_once('db_config');
+
+/**
+ * Provides single class for database connection
+ *
+ * @author William Moffitt
+ */
+class Connection {
+    
+    private $pdo;
+    
+    public function __construct() {
+        try {
+            $this->pdo = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+        } catch (PDOException $e) {
+            echo 'Error connecting to database: ' . $e->getMessage();
+            die();
+        }
+    }
+    
+    public function getPdo() {
+        return $this->pdo;
+    }
+    
+}
