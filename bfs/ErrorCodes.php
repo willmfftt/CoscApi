6eba8cd7c59de7c2785a36d84fa79001f9c6da54
William Moffitt <w.moffitt@wmoffitt.com>
2015-10-28 18:42:01 -0400

    Added user login

--- bfs/ErrorCodes.php
+++ bfs/ErrorCodes.php
@@ -12,5 +12,6 @@ class ErrorCodes {
     const USER_INCOMPLETE = 0;
     const USER_GENERIC_ERROR = 1;
     const USERNAME_INVALID = 2;
+    const PASSWORD_INVALID = 3;
     
 }
--- bfs/login/Login.php
+++ bfs/login/Login.php
@@ -1,10 +1,56 @@
 <?php
 
+namespace Bfs\Login;
+
+use Bfs\Crypto;
+use Bfs\ErrorCodes;
+use Bfs\Database\Connection;
+use Bfs\Database\User;
+use Bfs\Database\Dao\UserDao;
+
 /**
  * 
  *
  * @author William Moffitt
  */
 class Login {
+    
+    public function login(UserDao $userDao) {
+        if (!isset($userDao->password)) {
+            return array(
+                'error' => true,
+                'code'  => ErrorCodes::USER_INCOMPLETE,
+                'msg'   => "Missing information"
+            );
+        }
+        
+        $conn = new Connection();
+        $user = new User($conn->getPdo());
+        $result = $user->read($userDao);
+        
+        if (!($result instanceof UserDao)) {
+            return $result;
+        }
+        
+        $hashedPassword = Crypto::hashPassword($userDao->password, $result->salt);
+        if ($result->password == $hashedPassword) {
+            return array(
+                'error'        => false,
+                'id'           => $result->id,
+                'first_name'   => $result->first_name,
+                'last_name'    => $result->last_name,
+                'username'     => $result->username,
+                'dob'          => $result->dob,
+                'email'        => $result->email,
+                'is_moderator' => $result->is_moderator
+            );
+        } else {
+            return array(
+                'error' => true,
+                'code'  => ErrorCodes::PASSWORD_INVALID,
+                'msg'   => "Password invalid"
+            );
+        }
+    }
     
 }
